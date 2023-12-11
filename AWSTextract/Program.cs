8c8865117751a7d84c4427a64288706ba15aaeba
DonS <stiermandon@gmail.com>
2023-12-10 21:56:54 -0700

    initial load

---
+++ AWSTextract/Program.cs
@@ -0,0 +1,35 @@
+﻿// System packages
+using System.Threading.Tasks;
+using System;
+using System.Configuration;
+using Epic.AI.AWS.OCR;
+
+/*
+* need to run:
+* dotnet add package AWSSDK.Textract --version 3.7.300.16" from within the console project directory
+* dotnet add package AWSSDK.Core
+* dotnet add package AWSSDK.S3
+* dotnet add package System.Configuration.ConfigurationManager
+*/
+
+namespace AWSTextract
+{
+    internal class Program
+    {
+        private const string BUCKET = "hs-extract";
+        static async Task Main(string[] args)
+        {
+            var accessKey = ConfigurationManager.AppSettings["accessKey"];
+            var secretKey = ConfigurationManager.AppSettings["secretKey"];
+            var _awsCredentials = new Amazon.Runtime.BasicAWSCredentials(accessKey, secretKey);
+            var image_name = "C:\\Users\\Don\\Documents\\DataScience\\AWSProjects\\aws-textract\\AWSTextract\\bc1.png";
+            string outputText = "";
+            AWSTextractOCR.UploadFileToS3(image_name, _awsCredentials);
+            // this will allow you to run several of these at a time
+            // var tasks = new List<Task> { AnalyzeDocument(BUCKET, image_name) };
+            // await Task.WhenAll(tasks);
+            outputText = await AWSTextractOCR.AnalyzeDocument(BUCKET, image_name, _awsCredentials);
+            Console.WriteLine(outputText);
+        }
+    }
+}
---
+++ Epic.AI.AWS/OCR/AWSTextractOCR.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+// AWS packages
+using Amazon.Textract;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+
+namespace Epic.AI.AWS.OCR
+{
+    public class AWSTextractOCR
+    {
+        private const string BUCKET = "hs-extract";
+        public static void UploadFileToS3(string fileName, Amazon.Runtime.BasicAWSCredentials _awsCredentials)
+        {
+            using (var s3Client = new AmazonS3Client(_awsCredentials, Amazon.RegionEndpoint.USWest1))
+            {
+                var fileTransferUtility = new TransferUtility(s3Client);
+                using (Stream sourceStream = File.OpenRead(fileName))
+                {
+                    fileTransferUtility.Upload(sourceStream, BUCKET, "bc1_test1.png");
+                    Console.WriteLine(sourceStream);
+                }
+            }
+        }
+        public static async Task<string> AnalyzeDocument(string bucket, string file, Amazon.Runtime.BasicAWSCredentials _awsCredentials)
+        {
+            var featureTypes = new List<string>();
+            featureTypes.Add("FORMS");
+            string detectedText = "";
+            using (var client = new Amazon.Textract.AmazonTextractClient(_awsCredentials))
+            {
+                var request = new Amazon.Textract.Model.AnalyzeDocumentRequest
+                {
+                    Document = new Amazon.Textract.Model.Document()
+                    {
+                        S3Object = new Amazon.Textract.Model.S3Object()
+                        {
+                            Bucket = BUCKET,
+                            Name = "bc1_test1.png"
+                        }
+                    },
+                    FeatureTypes = featureTypes
+                };
+                var results = await client.AnalyzeDocumentAsync(request);
+                var lineBlocks = (from x in results.Blocks
+                                  where x.BlockType == BlockType.LINE
+                                  select x).ToList();
+                var wordBlocks = (from x in results.Blocks
+                                  where x.BlockType == BlockType.WORD
+                                  select x).ToList();
+                foreach (var block in lineBlocks)
+                {
+                    detectedText = detectedText + "\n" + block.Text;
+                    Console.WriteLine(block.Text);
+                }
+            }
+            return detectedText;
+        }
+    }
+}
